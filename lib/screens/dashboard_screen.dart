48dc5896236372284f9ec574acfe3837ea08c9f6
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-02 21:15:24 +0545

    dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,15 +1,123 @@
+import 'package:assignment_3/screens/area_of_circle_screen.dart';
+import 'package:assignment_3/screens/arithmetic_screen.dart';
+import 'package:assignment_3/screens/palindrome_screen.dart';
+import 'package:assignment_3/screens/simple_interest_screen.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatefulWidget {
+class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "DashBoard Screen",
+          style: TextStyle(
+            color: Colors.greenAccent,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: "Times New Roman",
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ArithmeticScreen()),
+                  );
+                },
+                child: Text(
+                  "Arithmetic Screen",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    fontFamily: "Times New Roman",
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AreaOfCircleScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Area of circle Screen",
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    fontFamily: "Times New Roman",
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SimpleInterestScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Simple Interest Screen",
+                  style: TextStyle(
+                    color: Colors.pinkAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    fontFamily: "Times New Roman",
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PalindromeScreen()),
+                  );
+                },
+                child: Text(
+                  "Palindrome Screen",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    fontFamily: "Times New Roman",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
