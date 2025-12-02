c62a23cb9bd0695f7731ae09cd986f3c2b42edac
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-02 21:12:19 +0545

    arithmetic

--- lib/screens/arithmetic_screen.dart
+++ lib/screens/arithmetic_screen.dart
@@ -1,17 +1,132 @@
+import 'package:assignment_3/widget/my_text_form_field.dart';
 import 'package:flutter/material.dart';
+
+import '../common/my_snackbar.dart';
+import '../widgets/my_button.dart';
+import '../widgets/my_text_form_field.dart';
 
 class ArithmeticScreen extends StatefulWidget {
   const ArithmeticScreen({super.key});
 
   @override
-  State<ArithmeticScreen> createState() => _ArithmeticScreenState();
+  State<ArithmeticScreen> createState() => _ArithmeticScreen();
 }
 
-class _ArithmeticScreenState extends State<ArithmeticScreen> {
+class _ArithmeticScreen extends State<ArithmeticScreen> {
+  final TextEditingController firstController = TextEditingController(
+    text: "1",
+  );
+  final TextEditingController secondController = TextEditingController(
+    text: "1",
+  );
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Arithmetic Operation',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: "Times New Roman",
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              MyTextFormField(
+                text: "Enter first number",
+                onChanged: (value) {
+                  firstController.text = value;
+                },
+              ),
+              const SizedBox(height: 10),
+
+              MyTextFormField(
+                text: "Enter second number",
+                onChanged: (value) {
+                  secondController.text = value;
+                },
+              ),
+              const SizedBox(height: 15),
+
+              MyButton(
+                text: "SUM",
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    int f = int.parse(firstController.text);
+                    int s = int.parse(secondController.text);
+                    int result = f + s;
+
+                    showMySnackBar(context: context, message: "Sum = $result");
+                  }
+                },
+              ),
+              const SizedBox(height: 10),
+
+              MyButton(
+                text: "SUBTRACT",
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    int f = int.parse(firstController.text);
+                    int s = int.parse(secondController.text);
+                    int result = f - s;
+
+                    showMySnackBar(
+                      context: context,
+                      message: "Subtract = $result",
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 10),
+
+              MyButton(
+                text: "MULTIPLY",
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    int f = int.parse(firstController.text);
+                    int s = int.parse(secondController.text);
+                    int result = f * s;
+
+                    showMySnackBar(
+                      context: context,
+                      message: "Multiplication = $result",
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 10),
+
+              MyButton(
+                text: "DIVIDE",
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    int f = int.parse(firstController.text);
+                    int s = int.parse(secondController.text);
+                    int result = f ~/ s;
+
+                    showMySnackBar(
+                      context: context,
+                      message: "Division = $result",
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
